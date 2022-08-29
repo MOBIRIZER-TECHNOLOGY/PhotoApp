e3dfb45c1863fad6ff3246c8508d866a9fe63fbd
pawan <pawan.ignou@gmail.com>
2022-08-29 08:50:33 +0530

    removed old unwanted files

---
+++ Pods/ToastViewSwift/Sources/Toast/Toast.swift
@@ -0,0 +1,206 @@
+//
+//  Toast.swift
+//  Toast
+//
+//  Created by Bastiaan Jansen on 27/06/2021.
+//
+
+import UIKit
+
+public class Toast {
+    private var closeTimer: Timer?
+    
+    /// This is for pan gesture to close.
+    private var startY: CGFloat = 0
+    private var startShiftY: CGFloat = 0
+    
+    public static var defaultImageTint: UIColor {
+        if #available(iOS 13.0, *) {
+            return .label
+        } else {
+            return .black
+        }
+    }
+    
+    public let view: ToastView
+    
+    private let config: ToastConfiguration
+    
+    private var initialTransform: CGAffineTransform {
+        return CGAffineTransform(scaleX: 0.9, y: 0.9).translatedBy(x: 0, y: -100)
+    }
+    
+    /// Creates a new Toast with the default Apple style layout with a title and an optional subtitle.
+    /// - Parameters:
+    ///   - title: Title which is displayed in the toast view
+    ///   - subtitle: Optional subtitle which is displayed in the toast view
+    ///   - config: Configuration options
+    /// - Returns: A new Toast view with the configured layout
+    public static func text(
+        _ title: String,
+        subtitle: String? = nil,
+        config: ToastConfiguration = ToastConfiguration()
+    ) -> Toast {
+        let view = AppleToastView(child: TextToastView(title, subtitle: subtitle))
+        return self.init(view: view, config: config)
+    }
+    
+    /// Creates a new Toast with the default Apple style layout with an icon, title and optional subtitle.
+    /// - Parameters:
+    ///   - image: Image which is displayed in the toast view
+    ///   - imageTint: Tint of the image
+    ///   - title: Title which is displayed in the toast view
+    ///   - subtitle: Optional subtitle which is displayed in the toast view
+    ///   - config: Configuration options
+    /// - Returns: A new Toast view with the configured layout
+    public static func `default`(
+        image: UIImage,
+        imageTint: UIColor? = defaultImageTint,
+        title: String,
+        subtitle: String? = nil,
+        config: ToastConfiguration = ToastConfiguration()
+    ) -> Toast {
+        let view = AppleToastView(
+            child: IconAppleToastView(image: image, imageTint: imageTint, title: title, subtitle: subtitle)
+        )
+        return self.init(view: view, config: config)
+    }
+    
+    /// Creates a new Toast with a custom view
+    /// - Parameters:
+    ///   - view: A view which is displayed when the toast is shown
+    ///   - config: Configuration options
+    /// - Returns: A new Toast view with the configured layout
+    public static func custom(
+        view: ToastView,
+        config: ToastConfiguration = ToastConfiguration()
+    ) -> Toast {
+        return self.init(view: view, config: config)
+    }
+    
+    /// Creates a new Toast with a custom view
+    /// - Parameters:
+    ///   - view: A view which is displayed when the toast is shown
+    ///   - config: Configuration options
+    /// - Returns: A new Toast view with the configured layout
+    public required init(view: ToastView, config: ToastConfiguration) {
+        self.config = config
+        self.view = view
+        
+        view.transform = initialTransform
+        if config.enablePanToClose {
+            enablePanToClose()
+        }
+    }
+    
+    /// Show the toast with haptic feedback
+    /// - Parameters:
+    ///   - type: Haptic feedback type
+    ///   - time: Time after which the toast is shown
+    public func show(haptic type: UINotificationFeedbackGenerator.FeedbackType, after time: TimeInterval = 0) {
+        UINotificationFeedbackGenerator().notificationOccurred(type)
+        show(after: time)
+    }
+    
+    /// Show the toast
+    /// - Parameter delay: Time after which the toast is shown
+    public func show(after delay: TimeInterval = 0) {
+        config.view?.addSubview(view) ?? topController()?.view.addSubview(view)
+        view.createView(for: self)
+        
+        UIView.animate(withDuration: config.animationTime, delay: delay, options: [.curveEaseOut, .allowUserInteraction]) {
+            self.view.transform = .identity
+        } completion: { [self] _ in
+            closeTimer = Timer.scheduledTimer(withTimeInterval: .init(config.displayTime), repeats: false) { [self] _ in
+                if config.autoHide {
+                    close()
+                }
+            }
+        }
+    }
+    
+    /// Close the toast
+    /// - Parameters:
+    ///   - completion: A completion handler which is invoked after the toast is hidden
+    public func close(completion: (() -> Void)? = nil) {
+        UIView.animate(withDuration: config.animationTime,
+                       delay: 0,
+                       options: [.curveEaseIn, .allowUserInteraction],
+                       animations: {
+            self.view.transform = self.initialTransform
+        }, completion: { _ in
+            self.view.removeFromSuperview()
+            completion?()
+        })
+    }
+    
+    private func topController() -> UIViewController? {
+        let keyWindow = UIApplication.shared.windows.filter {$0.isKeyWindow}.first
+        
+        if var topController = keyWindow?.rootViewController {
+            while let presentedViewController = topController.presentedViewController {
+                topController = presentedViewController
+            }
+            return topController
+        }
+        return nil
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+public extension Toast{
+    private func enablePanToClose() {
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(toastOnPan))
+        self.view.addGestureRecognizer(pan)
+    }
+    
+    @objc private func toastOnPan(_ gesture: UIPanGestureRecognizer) {
+        guard let topVc = topController() else {
+            return
+        }
+        
+        switch gesture.state{
+        case .began:
+            startY = self.view.frame.origin.y
+            startShiftY = gesture.location(in: topVc.view).y
+            closeTimer?.invalidate() // prevent timer to fire close action while being touched
+        case .changed:
+            let delta = gesture.location(in: topVc.view).y - startShiftY
+            if delta <= 0{
+                self.view.frame.origin.y = startY + delta
+            }
+        case .ended:
+            let threshold = initialTransform.ty + (startY - initialTransform.ty) * 2 / 3
+            
+            if self.view.frame.origin.y < threshold {
+                close()
+            }else{
+                // move back to origin position
+                UIView.animate(withDuration: config.animationTime, delay: 0, options: [.curveEaseOut, .allowUserInteraction]) {
+                    self.view.frame.origin.y = self.startY
+                } completion: { [self] _ in
+                    closeTimer = Timer.scheduledTimer(withTimeInterval: .init(config.displayTime), repeats: false) { [self] _ in
+                        if config.autoHide {
+                            close()
+                        }
+                    }
+                }
+            }
+        default:
+            break
+        }
+    }
+    
+    func enableTapToClose() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(toastOnTap))
+        self.view.addGestureRecognizer(tap)
+    }
+    
+    @objc func toastOnTap(_ gesture: UITapGestureRecognizer) {
+        closeTimer?.invalidate()
+        close()
+    }
+}
